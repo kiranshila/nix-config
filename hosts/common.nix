ebee27b0b0317679486c364fac91bd00aff3789b
Kiran Shila <me@kiranshila.com>
2024-06-02 20:02:44 -0700

    More work on libvirt

--- hosts/common.nix
+++ hosts/common.nix
@@ -239,21 +239,24 @@
   };
 
   # Virtualization
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          })
-          .fd
-        ];
+  virtualisation = {
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            })
+            .fd
+          ];
+        };
       };
     };
   };
--- hosts/kix.nix
+++ hosts/kix.nix
@@ -35,6 +35,28 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
+  # Custom boot settings
+  boot = {
+    # Enable IOMMU for libvirt and isolate the gpu we'll pass through
+    kernelParams = [
+      "intel_iommu=on"
+      "iommu=pt"
+      "vfio-pci.ids=10de:1c31,10de:10f1"
+    ];
+
+    # VFIO kernel modules
+    initrd.kernelModules = [
+      # Explicitly load VFIO before nvidia so the driver doesn't grab control before vfio
+      "vfio_pci"
+      "vfio"
+      "vfio_iommu_type1"
+
+      #      "nvidia"
+      #      "nvidia_uvm"
+      #    "nvidia_drm"
+    ];
+  };
+
   # NixOS "State Version"
   system.stateVersion = "24.05";
 }
