977c3582b226a111832f7b82a94f61d5ebd0f061
Kiran Shila <me@kiranshila.com>
2024-06-02 17:40:26 -0700

    Add nix-index

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -15,6 +15,9 @@
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
+  # Enable nix-index
+  programs.nix-index.enable = true;
+
   # Enable Direnv
   programs.direnv.enable = true;
 
