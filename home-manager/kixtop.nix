4589b984f8c7515a816b0d5a152f70160ad68ed4
Kiran Shila <me@kiranshila.com>
2024-09-03 11:33:39 -0700

    VPN in host, not home

--- home-manager/kixtop.nix
+++ home-manager/kixtop.nix
@@ -17,27 +17,6 @@
 
   # Use the pgtk build as kixtop is on wayland
   programs.emacs.package = pkgs.emacs-gtk;
-  # VPN
-  networking = {
-    firewall = {
-      allowedUDPPorts = [51820];
-    };
-    wireguard.interfaces = {
-      wg0 = {
-        ips = ["10.10.10.3/24"];
-        listenPort = 51820;
-        privateKeyFile = "/home/kiran/sync/wireguard/kixtop_private";
-        peers = [
-          {
-            publicKey = "ir9VitpGaipQ3183crPzbI0D4JTNR+lKZmuv2CnG51U=";
-            allowedIPs = ["0.0.0.0/0"];
-            endpoint = "vpn.kiranshila.com:51820";
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
-  };
 
   # NixOS State Version for Home
   home.stateVersion = "23.11";
--- hosts/kixtop.nix
+++ hosts/kixtop.nix
@@ -51,6 +51,28 @@
     };
   };
 
+  # VPN
+  networking = {
+    firewall = {
+      allowedUDPPorts = [51820];
+    };
+    wireguard.interfaces = {
+      wg0 = {
+        ips = ["10.10.10.3/24"];
+        listenPort = 51820;
+        privateKeyFile = "/home/kiran/sync/wireguard/kixtop_private";
+        peers = [
+          {
+            publicKey = "ir9VitpGaipQ3183crPzbI0D4JTNR+lKZmuv2CnG51U=";
+            allowedIPs = ["0.0.0.0/0"];
+            endpoint = "vpn.kiranshila.com:51820";
+            persistentKeepalive = 25;
+          }
+        ];
+      };
+    };
+  };
+
   # NixOS "State Version"
   system.stateVersion = "23.11";
 }
