64b4418101384d8fc7feb0c5dcd39f17deac223f
Kiran Shila <me@kiranshila.com>
2026-02-01 16:34:21 -0800

    fix: graal

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -84,10 +84,7 @@
   # Minecraft
   home.packages = with pkgs; [
     (prismlauncher.override {
-      additionalPrograms = [ffmpeg];
-      jdks = [
-        graalvm-ce
-      ];
+      additionalPrograms = [ffmpeg]; # Some mods need it
     })
   ];
 
