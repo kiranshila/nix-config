568374b57acaaef29075d703213eba05ed5dc2c2
Kiran Shila <me@kiranshila.com>
2026-03-09 21:06:38 -0700

    fix: revert openrct2, kix only

--- home/modules/apps/openrct2.nix
+++ home/modules/apps/openrct2.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [(config.lib.nixGL.wrap openrct2)];
+  home.packages = with pkgs; [openrct2];
   home.file.".config/OpenRCT2/config.ini".text = lib.generators.toINI {} {
     general = {
       game_path = "/mnt/hdd/SteamLibrary/steamapps/common/Rollercoaster Tycoon 2/";
