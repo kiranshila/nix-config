6c6709f80d0495cf8bed8a4b72385eabff0d0a17
Kiran Shila <me@kiranshila.com>
2026-04-08 11:34:50 -0700

    fix: kicad version

--- home/modules/apps/kicad.nix
+++ home/modules/apps/kicad.nix
@@ -2,23 +2,24 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   homeDir = config.home.homeDirectory;
   # no 3d moodels to save the nix store
   # Use non-compressed 3d models for step export
   myKicad = pkgs.kicad-small.override {
     compressStep = false;
   };
-in {
+in
+{
   home.packages = [
-    (config.lib.nixGL.wrap
-      (myKicad.overrideAttrs (oldAttrs: {
-        makeWrapperArgs =
-          oldAttrs.makeWrapperArgs
-          ++ [
-            "--set-default KICAD_DSA_LIBRARY ${homeDir}/sync/Projects/PCB/dsa2klib"
-            "--set-default KICAD9_3DMODEL_DIR ${homeDir}/.local/share/kicad/kicad-packages3D"
-          ];
-      })))
+    (config.lib.nixGL.wrap (
+      myKicad.overrideAttrs (oldAttrs: {
+        makeWrapperArgs = oldAttrs.makeWrapperArgs ++ [
+          "--set-default KICAD_DSA_LIBRARY ${homeDir}/sync/Projects/PCB/dsa2klib"
+          "--set-default KICAD10_3DMODEL_DIR ${homeDir}/.local/share/kicad/kicad-packages3D"
+        ];
+      })
+    ))
   ];
 }
